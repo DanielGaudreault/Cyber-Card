04b4d21460ae1ae9d3308f138b3239fb9a384e3c
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-10-23 10:31:11 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,7 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/images/capmatic.png',
-  '/images/logo.png'
+  '/images/capmatic.png'
 ];
 
 self.addEventListener('install', (event) => {
